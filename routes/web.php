2c1a06b10ef84beddc204e0a5d8be831328ac7b0
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2021-11-26 09:03:45 +0700

    feat(option): add batch handler

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    option()->put([
+        'school-site',
+        // 'school-instagram' => 'amann'
+    ]);
 });
 
 Route::prefix('panel')->name('panel.')->middleware('auth')->group(function() {
